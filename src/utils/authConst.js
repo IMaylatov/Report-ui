973c8c717a2c262b5f3b04b96581b268b6aa6745
Майлатов Иван <imaylatov@softrust.ru>
2020-10-24 01:00:24 +0300

    get access token

--- src/utils/authConst.js
+++ src/utils/authConst.js
@@ -2,7 +2,7 @@ export const IDENTITY_CONFIG = {
   authority: process.env.REACT_APP_AUTH_URL,
   client_id: process.env.REACT_APP_IDENTITY_CLIENT_ID,
   redirect_uri: process.env.REACT_APP_REDIRECT_URL,
-  responseType: "id_token token",
+  response_type: "id_token token",
   scope: "openid profile softrust_report_api",
   post_logout_redirect_uri: process.env.REACT_APP_LOGOFF_REDIRECT_URL
 };
